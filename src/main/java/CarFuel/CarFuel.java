695062112c7b6dc5f03a68dbd4e40aaedb4423f7
colonel8377 <80370342+colonel8377@users.noreply.github.com>
2021-10-18 17:38:23 +0800

    1.(feat):add image

--- src/main/java/CarFuel/CarFuel.java
+++ src/main/java/CarFuel/CarFuel.java
@@ -1,36 +1,83 @@
 package CarFuel;
+
+import java.util.Arrays;
 import java.util.PriorityQueue;
+
 public class CarFuel {
     public static void main(String[] args) {
         // Add boundary condition.
-        // Set prices[-1] = 0 since there is no cost to add our mug at city A.
-        // Set distance[0] = 0 and distance[n + 1] = dist(A, B) according to problem.
-        int res = findMinPrice(new int[]{0, 2, 3, 0, 1, 0, 2, 1, 2}, new int[]{0, 2, 4, 5, 6, 7, 9, 11, 14, 15}, 2);
+        // Set p[-1] = 0 since there is no dp to add our mug at city A.
+        // Set c[0] = 0 and c[n + 1] = dist_ab(A, B) according to problem.
+        int res = findMinPrice2(new int[]{3, 0, 0, 0, 0, 0, 1, 2, 4, 2, 2, 12, 0, 2, 0}, new int[]{1, 3, 5, 7, 9, 11, 13, 15, 17, 14, 16, 17, 18, 20, 21}, 1, 22);
         System.out.println("" + res);
     }
-    public  static int findMinPrice(int[] prices, int[] distance, int d){
-        int[] cost = new int[prices.length + 1];
-        cost[0] = 0;
+
+    public static int findMinPrice1(int[] p, int[] c, int d) {
+        int[] dp = new int[p.length + 1];
+        dp[0] = 0;
         int k = 0;
         int price;
         PriorityQueue pq = new PriorityQueue<Integer>();
-        for(int i = 0; i < prices.length + 1 ; i ++){
-            while(distance[i] - distance[k] > d && i > k) {
-                pq.remove(cost[k]);
-                k ++;
+        for (int i = 0; i < p.length + 1; i++) {
+            while (c[i] - c[k] > d && i > k) {
+
+                pq.remove(dp[k]);
+                k++;
             }
-            if(k >= i && k != 0){
+            if (k >= i && k != 0) {
                 return -1;
             }
-            price = i < prices.length ? prices[i] : 0;
-            if(pq.size() > 0){
-                cost[i] = (Integer)pq.peek() + price;
+            price = i < p.length ? p[i] : 0;
+            if (pq.size() > 0) {
+                dp[i] = (Integer) pq.peek() + price;
+            } else {
+                dp[i] = price;
             }
-            else{
-                cost[i] = price;
-            }
-            pq.add(cost[i]);
+            pq.add(dp[i]);
         }
-        return cost[prices.length];
+        return dp[p.length];
+    }
+
+    public static int findMinPrice2(int[] p, int[] c, int d, int dist_ab) {
+        int[] dp = new int[p.length];
+        Arrays.fill(dp, Integer.MIN_VALUE);
+        dp[0] = 0;
+        int pointer = 0;
+        int count = 0;
+
+        for (int i = 1; i < p.length; i++) {
+            if (c[i - 1] + d < c[i]) {
+                System.out.println("==============" + count++ + "==============");
+                return -1;
+            } else if (d >= c[i]) {
+                dp[i] = 0;
+                System.out.println("==============" + count++ + "==============");
+            } else {
+                for (int j = pointer; j < i; j++) {
+                    if (c[j] + d >= c[i]) {
+                        System.out.println("==============" + count++ + "==============");
+                        dp[i] = dp[j] + p[j];
+                        pointer = j;
+                        break;
+                    } else {
+                        System.out.println("==============" + count++ + "==============");
+                        pointer += 1;
+                    }
+                }
+            }
+            if (dp[i] + p[i] < dp[i - 1] + p[i - 1]) {
+                System.out.println("==============" + count++ + "==============");
+                pointer = i;
+            }
+        }
+        if (c[c.length - 1] + d < dist_ab) {
+            return -1;
+        }
+        for (int j = pointer; j < p.length; j++) {
+            if(c[j] + d >= dist_ab){
+                return dp[j] + p[j];
+            }
+        }
+        return -1;
     }
 }
--- src/main/java/MatchString/KMP.java
+++ src/main/java/MatchString/KMP.java
@@ -1,18 +1,39 @@
 package MatchString;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class KMP {
     public static void main(String[] args) {
-        System.out.println(indexOf("abaabaacaba", "abaabaacaba"));
+//        System.out.println(indexOf("abaabaacaba", "aac"));
+//        getNext03("abaabaacaba".toCharArray());
+        int q = -1;
+        List<Integer> res = new ArrayList<>();
+        char[] t = "abaabaacaba".toCharArray();
+        char[] p = "aac".toCharArray();
+        int[] next = new int[]{-1, 0, -1};
+        for (int i = 0; i < 9; i++) {
+            while (q >= 0 &&  p[q + 1] != t[i]) {
+                q = next[q];
+            }
+            if(p[q + 1] == t[i]){
+                q = q + 1;
+            }
+            if(q == 2){
+                res.add(i - 2 + 1);
+                q = next[q];
+            }
+        }
     }
+
     public static int indexOf(String source, String pattern) {
         int i = 0, j = 0;
         char[] src = source.toCharArray();
         char[] ptn = pattern.toCharArray();
         int sLen = src.length;
         int pLen = ptn.length;
-        int[] next = getNext(ptn);
+        int[] next = getNext01(ptn);
         while (i < sLen && j < pLen) {
             if (j == -1 || src[i] == ptn[j]) {
                 i++;
@@ -25,7 +46,73 @@ public class KMP {
             return i - j;
         return -1;
     }
-    public static int[] getNext(char[] p) {
+
+    public static void getNext04(char[] p) {
+        int[] next = new int[p.length + 1];
+        for (int i = 1; i < p.length; i++) {
+            int j = next[i + 1];
+
+            while (j > 0 && p[j] != p[i]) {
+                j = next[j];
+            }
+
+            if (j > 0 || p[j] == p[i]) {
+                next[i + 1] = j + 1;
+            }
+        }
+
+    }
+
+    public static void getNext03(char[] p) {
+        int n = p.length;
+        int[] pi = new int[n];
+        pi[0] = 0;
+        int q = 1;
+        int k = -1;
+        for (int i = 1; i < n; i++) {
+            while (q > 0 && p[q] != p[i]) {
+                q = pi[q - 1];
+            }
+
+            if (p[q] == p[i]) {
+                q++;
+            }
+
+            pi[i] = q;
+        }
+        for (int ne : pi) {
+            System.out.print(" " + ne);
+        }
+    }
+
+    public static int[] getNext02(char[] p) {
+        int mLength = p.length;
+        int[] next = new int[p.length];
+        next[0] = -1;
+        int pre = -1; //next[current-1]
+        int current = 0;
+        int count = 1;
+        while (current < mLength - 1) { //pre表示前缀,current表示后缀
+            if (pre == -1 || p[current] == p[pre]) { //若p[k]=p[j],则 next[j+1]=next[j]+1;
+                ++pre;
+                ++current;
+                next[current] = pre;
+
+            } else {
+                pre = next[pre];
+            }
+            System.out.print("Loops " + count + " : ");
+            for (int ne : next) {
+                System.out.print(" " + ne);
+            }
+            System.out.println();
+            count++;
+        }
+
+        return next;
+    }
+
+    public static int[] getNext01(char[] p) {
         int pLen = p.length;
         int[] next = new int[pLen];
         Arrays.fill(next, -2);
@@ -46,11 +133,11 @@ public class KMP {
                 k = next[k];
             }
             System.out.print("Loops " + count + " : ");
-            for(int ne : next){
+            for (int ne : next) {
                 System.out.print(" " + ne);
             }
             System.out.println();
-            count ++;
+            count++;
         }
         return next;
     }
